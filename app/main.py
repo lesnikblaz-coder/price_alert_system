6d2b73ae37207f31ef7363f3ad9e0776d9fbe20c
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-05 12:57:02 +0200

    lay out initial architecture for main.py -> user login/register/token endpoints

--- app/main.py
+++ app/main.py
@@ -1,3 +1,25 @@
-"""
-FastAPI endpoints
-"""
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordRequestForm
+
+from app import schemas
+from app.dependencies import SessionDep
+
+
+
+# app lifespan
+
+
+app = FastAPI()
+
+
+@app.post("/auth/login", response_model=schemas.TokenResponse)
+async def login(session: SessionDep, request: schemas.LoginRequest) -> schemas.TokenResponse:
+    ...
+
+@app.post("/auth/register", response_model=schemas.TokenResponse)
+async def register(session: SessionDep, request: schemas.RegisterRequest) -> schemas.TokenResponse:
+    ...
+
+@app.post("/auth/token", response_model=schemas.TokenResponse)
+async def token(session: SessionDep, form_data: OAuth2PasswordRequestForm = Depends()) -> schemas.TokenResponse:
+    ...
