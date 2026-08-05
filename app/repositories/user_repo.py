affb971c5801c7ba2df7f0f5fd0ec8707b94e5f7
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-05 15:39:05 +0200

    feat(user): remove unnecessary eager loading for alerts in basic get_by_id function, add get_by_email, create and get_by_id_with_alerts queries

--- app/repositories/user_repo.py
+++ app/repositories/user_repo.py
@@ -10,7 +10,29 @@ class UserRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
+
     async def get_by_id(self, user_id: UUID) -> User | None:
+        result = await self.session.execute(
+            select(User)
+            .where(User.id == user_id)
+        )
+
+        return result.scalar_one_or_none()
+
+    async def get_by_email(self, email: str) -> User | None:
+        result = await self.session.execute(
+            select(User).where(User.email == email)
+        )
+
+        return result.scalar_one_or_none()
+
+    async def create(self, user: User) -> User:
+        self.session.add(user)
+        await self.session.commit()
+        await self.session.refresh(user)
+        return user
+
+    async def get_by_id_with_alerts(self, user_id: UUID) -> User | None:
         result = await self.session.execute(
             select(User)
             .where(User.id == user_id)
