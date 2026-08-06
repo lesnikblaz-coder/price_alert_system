1dce1025172709dc0259a195e6c1fcb6619b8a1e
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-06 10:34:04 +0200

    feat(auth): create AuthService class, initialize register and login endpoints, refactor and move get_current_user so it's owned by AuthService

--- app/services/auth_services.py
+++ app/services/auth_services.py
@@ -1,5 +1,58 @@
+from app import auth
+from app.exceptions import custom
 from app.repositories.user_repo import UserRepository
+from app.models import User
+from app.schemas import TokenResponse
+from app.logging_config import logger
 
 
-async def register(user_repo: UserRepository, email: str, password: str) -> None:
-    if await user_repo.
+class AuthService:
+    def __init__(self, user_repo: UserRepository):
+        self.repo = user_repo
+
+    async def register(self, email: str, password: str) -> TokenResponse:
+        if await self.repo.get_by_email(email):
+            raise custom.DuplicateEmailError()
+
+        user = User(
+            email=email,
+            hashed_pw=auth.get_pw_hash(password)
+        )
+
+        user = await self.repo.create(user)
+
+        logger.info(
+            "New user registered | id:%s, email:%s", user.id, user.email
+        )
+
+        return TokenResponse(
+            access_token=auth.get_token(user.id)
+        )
+
+    async def login(self, email: str, password: str) -> TokenResponse:
+        user = await self.repo.get_by_email(email)
+
+        if not user or not auth.verify_pw(password, user.hashed_pw):
+            logger.warning(
+                "Invalid login attempt for email:%s", email
+            )
+
+            raise custom.InvalidCredentialsError()
+
+        logger.info(
+            "User login successful | id:%s, email:%s", user.id, user.email
+        )
+
+        return TokenResponse(
+            access_token=auth.get_token(user.id)
+        )
+
+    async def get_current_user(self, token: str) -> User:
+        user_id = auth.decode_token(token)
+
+        user = await self.repo.get_by_id(user_id)
+
+        if not user:
+            raise custom.InvalidCredentialsError()
+
+        return user
