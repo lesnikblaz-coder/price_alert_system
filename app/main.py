adbd201099ebc4b95a8b18f90a4761c53e5fb9c0
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-06 19:46:32 +0200

    clean up and layer code, add get alerts and create alert endpoints, individualized for each user

--- app/main.py
+++ app/main.py
@@ -2,8 +2,9 @@ from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app import schemas
-from app.dependencies import AuthServiceDep
+from app.dependencies import AuthServiceDep, CurrentUserDep, AlertServiceDep
 from app.exceptions.handlers import register_exception_handlers
+from app.models import Alert
 
 
 # app lifespan
@@ -13,10 +14,13 @@ app = FastAPI()
 register_exception_handlers(app)
 
 
+# ---------- Root ----------
 @app.get("/", include_in_schema=False)
 def root() -> dict[str, str]:
     return {"status": "ok"}
 
+
+# ---------- Auth ----------
 @app.post("/auth/login", response_model=schemas.TokenResponse)
 async def login(
         service: AuthServiceDep,
@@ -45,4 +49,30 @@ async def token(
     return await service.login(
         form_data.username,
         form_data.password
+    )
+
+
+# ---------- Users (admin*) ----------
+#@app.get("/users")
+
+
+# ---------- Alerts ----------
+@app.get("/alerts", response_model=list[schemas.AlertResponse])
+async def get_alerts(
+        service: AlertServiceDep,
+        user: CurrentUserDep
+) -> list[Alert]:
+    return await service.get_by_user(user.id)
+
+@app.post("/alerts", response_model=schemas.AlertResponse)
+async def create_alert(
+        service: AlertServiceDep,
+        user: CurrentUserDep,
+        request: schemas.AlertRequest = Depends()
+) -> Alert:
+    return await service.create(
+        user_id=user.id,
+        symbol=request.symbol,
+        condition=request.condition,
+        target_price=request.target_price
     )
