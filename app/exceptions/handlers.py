518ffb2c4ae4be5f59071919b7136ff692def93b
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-06 10:32:03 +0200

    fix(exception handling): wrap the base exception handler to register it in main.py, change log level from exception to error

--- app/exceptions/handlers.py
+++ app/exceptions/handlers.py
@@ -1,24 +1,24 @@
-from fastapi import Request
+from fastapi import Request, FastAPI
 from fastapi.responses import JSONResponse
 
-from app.main import app
 from app.logging_config import logger
 from app.exceptions.base import AppException
 
 
-@app.exception_handler(AppException)
-async def app_exception_handler(request: Request, exc: AppException):
+def register_exception_handlers(app: FastAPI):
+    @app.exception_handler(AppException)
+    def app_exception_handler(request: Request, exc: AppException):
 
-    logger.exception(
-        "Exception on %s %s: %s",
-        request.method,
-        request.url.path,
-        exc
-    )
+        logger.error(
+            "Exception on %s %s: %s",
+            request.method,
+            request.url.path,
+            exc
+        )
 
-    return JSONResponse(
-        status_code=exc.status_code,
-        content={
-            "detail": str(exc)
-        }
-    )
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={
+                "detail": str(exc)
+            }
+        )
