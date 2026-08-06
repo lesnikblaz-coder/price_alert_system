5621f663c48ea4957a25db88f598dc6afa2e6ac7
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-06 19:42:25 +0200

    clean up and layer code, add alert repository and service dependencies

--- app/dependencies.py
+++ app/dependencies.py
@@ -5,11 +5,15 @@ from typing import Annotated
 from app import auth
 from app.database import AsyncSessionLocal
 from app.repositories.user_repo import UserRepository
+from app.repositories.alert_repo import AlertRepository
 from app.services.auth_services import AuthService
+from app.services.alert_service import AlertService
 from app.models import User
 from app.exceptions import custom
 
 
+
+# ---------- Session ----------
 async def get_session():
     async with AsyncSessionLocal() as session:
         yield session
@@ -17,19 +21,31 @@ async def get_session():
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
 
+
+# ---------- Repositories ----------
 async def get_user_repository(session: SessionDep) -> UserRepository:
     return UserRepository(session)
 
+async def get_alert_repository(session: SessionDep) -> AlertRepository:
+    return AlertRepository(session)
+
 UserRepoDep = Annotated[UserRepository, Depends(get_user_repository)]
+AlertRepoDep = Annotated[AlertRepository, Depends(get_alert_repository)]
 
 
-
+# ---------- Services ----------
 async def get_auth_service(user_repo: UserRepoDep) -> AuthService:
     return AuthService(user_repo)
 
+async def get_alert_service(alert_repo: AlertRepoDep) -> AlertService:
+    return AlertService(alert_repo)
+
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
+AlertServiceDep = Annotated[AlertService, Depends(get_alert_service)]
 
 
+
+# ---------- Auth ----------
 TokenDep = Annotated[str, Depends(auth.oauth2_scheme)]
 
 async def get_current_user(
