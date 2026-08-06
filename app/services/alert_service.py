903abe5b1e1b778f7fb5901165382ebb82acc097
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-06 19:44:57 +0200

    add alert service layer

---
+++ app/services/alert_service.py
@@ -0,0 +1,46 @@
+from uuid import UUID
+from decimal import Decimal
+
+from app.enums import AlertCondition
+from app.repositories.alert_repo import AlertRepository
+from app.models import Alert
+from app.exceptions import custom
+
+
+class AlertService:
+    def __init__(self, alert_repo: AlertRepository):
+        self.repo = alert_repo
+
+    async def get_by_user(self, user_id: UUID) -> list[Alert]:
+        return list(await self.repo.get_by_user(user_id))
+
+    async def get_by_id(self, alert_id: UUID) -> Alert:
+        alert = await self.repo.get_by_id(alert_id)
+
+        if not alert:
+            raise custom.AlertNotFoundError()
+
+        return alert
+
+    async def create(
+            self,
+            user_id: UUID,
+            symbol: str,
+            condition: AlertCondition,
+            target_price: Decimal
+    ) -> Alert:
+        alert = Alert(
+            user_id=user_id,
+            symbol=symbol.upper(),
+            condition=condition,
+            target_price=target_price
+        )
+
+        return await self.repo.create(alert)
+
+    async def get_active_by_symbol(self, symbol: str) -> list[Alert]:
+        return list(
+            await self.repo.get_active_by_symbol(
+                symbol
+            )
+        )
