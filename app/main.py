aa430f31e1fe31a98248a0bf23b994e428f2dd42
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-06 10:37:59 +0200

    register exception handlers, initialize auth endpoints: login, register and token

--- app/main.py
+++ app/main.py
@@ -2,24 +2,43 @@ from fastapi import FastAPI, Depends
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app import schemas
-from app.dependencies import SessionDep
-
+from app.dependencies import AuthServiceDep
+from app.exceptions.handlers import register_exception_handlers
 
 
 # app lifespan
 
 
 app = FastAPI()
+register_exception_handlers(app)
 
 
 @app.post("/auth/login", response_model=schemas.TokenResponse)
-async def login(session: SessionDep, request: schemas.LoginRequest) -> schemas.TokenResponse:
-    ...
+async def login(
+        service: AuthServiceDep,
+        request: schemas.LoginRequest
+) -> schemas.TokenResponse:
+    return await service.login(
+        request.email,
+        request.password
+    )
 
 @app.post("/auth/register", response_model=schemas.TokenResponse)
-async def register(session: SessionDep, request: schemas.RegisterRequest) -> schemas.TokenResponse:
-    ...
+async def register(
+        service: AuthServiceDep,
+        request: schemas.RegisterRequest
+) -> schemas.TokenResponse:
+    return await service.register(
+        request.email,
+        request.password
+    )
 
 @app.post("/auth/token", response_model=schemas.TokenResponse)
-async def token(session: SessionDep, form_data: OAuth2PasswordRequestForm = Depends()) -> schemas.TokenResponse:
-    ...
+async def token(
+        service: AuthServiceDep,
+        form_data: OAuth2PasswordRequestForm = Depends()
+) -> schemas.TokenResponse:
+    return await service.login(
+        form_data.username,
+        form_data.password
+    )
