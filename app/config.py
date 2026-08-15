928bddf606eab008176a3d8d8e301a2958466044
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-15 23:11:23 +0200

    change database_url host to use dockers postgres database instead of localhost

--- app/config.py
+++ app/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     db_user: str
     db_password: str
     db_host: str
+    db_host_postgres: str
     db_port: int = 5432
     db_name: str
 
@@ -27,7 +28,7 @@ class Settings(BaseSettings):
     def database_url(self) -> str:
         return (
             f"postgresql+asyncpg://{self.db_user}:{self.db_password}"
-            f"@{self.db_host}:{self.db_port}/{self.db_name}"
+            f"@{self.db_host_postgres}:{self.db_port}/{self.db_name}"
         )
 
 settings = Settings()
