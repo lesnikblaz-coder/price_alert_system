bde0dd616f4e83d3307503ef2f92ebad42a97758
Blaž Lešnik <lesnik.blaz@gmail.com>
2026-08-16 20:17:04 +0200

    add NullPool poolclass so the connections aren't retained by a pool and subsequently reused by another event loop (Celery worker's check_prices task

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.pool import NullPool
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
 
 from app.config import settings
@@ -11,7 +12,8 @@ class Base(AsyncAttrs, DeclarativeBase):
 engine = create_async_engine(
     settings.database_url,
     echo=False,
-    pool_pre_ping=True
+    pool_pre_ping=True,
+    poolclass=NullPool # connections aren't retained by a pool and subsequently reused by another event loop
 )
 
 AsyncSessionLocal = async_sessionmaker(
